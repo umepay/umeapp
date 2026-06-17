042b0e0232e759b14260807e49244442bd42bc3c
agrimensuraumepay <agrimensuraumepay@gmail.com>
2026-06-16 22:58:08 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,18 @@
-const CACHE = 'umeapp-v2';
+const CACHE = 'umeapp-v3';
 const CORE = [
-  './', './index.html', './mapa.html', './cartelera.html', './gas.html',
-  './manifest.json', './icon-192.png', './icon-512.png'
+  './', './index.html', './mapa.html', './bomberos.html', './cartelera.html', './gas.html',
+  './manifest.json', './icon-192.png', './icon-512.png',
+  './assets/centro-umepay.jpg'
 ];
-
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
       .then(() => self.clients.claim())
   );
 });
-
 self.addEventListener('fetch', e => {
   const req = e.request;
   if (req.method !== 'GET') return;            // no tocar los envíos (pedidos de gas, etc.)
