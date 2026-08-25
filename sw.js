26156bb359bc942476c712a795c9c0d37c4b8563
umepay <uumepay@gmail.com>
2026-08-25 08:44:44 -0300

    Capas agua: actualizar calles (LongTramo recalculado), cruces y mangueras + cache v8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'umeapp-v7';
+const CACHE = 'umeapp-v8';
 const CORE = [
   './', './index.html', './mapa.html', './bomberos.html', './cartelera.html', './gas.html', './umeblog.html',
   './acceso.js', './assets/oms.min.js',
