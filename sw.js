5c273f7e5941fcb7da3703deef77329f8a400d34
umepay <uumepay@gmail.com>
2026-08-25 08:52:28 -0300

    Capa cruces agua/calle: re-exportar corregido (40 cruces) + cache v9

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'umeapp-v8';
+const CACHE = 'umeapp-v9';
 const CORE = [
   './', './index.html', './mapa.html', './bomberos.html', './cartelera.html', './gas.html', './umeblog.html',
   './acceso.js', './assets/oms.min.js',
